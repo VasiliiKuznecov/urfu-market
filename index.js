0071e419a7888015aa6f9dd6d054f8b1443f7286
Vasilii Kuznetsov <kvas@yandex-team.ru>
2017-09-10 19:53:47 +0500

    body-parser

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
+var bodyParser = require('body-parser')
 var express = require("express");
 var app = express();
 app.use(express.logger());
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
 app
     .get('/buy', function(req, res) {
         res.send(JSON.stringify(req.query));
     })
     .post('/buy', function(req, res) {
-        res.send(JSON.stringify(req.query))
+        res.send(JSON.stringify(req.body))
     });
 
 var port = process.env.PORT || 5000;
