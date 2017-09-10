c5cfd368b86fe710b1d791e304d3c3856e044353
Vasilii Kuznetsov <kvas@yandex-team.ru>
2017-09-10 19:47:54 +0500

    show-query

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ var express = require("express");
 var app = express();
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+app
+    .get('/buy', function(req, res) {
+        res.send(JSON.stringify(req.query));
+    })
+    .post('/buy', function(req, res) {
+        res.send(JSON.stringify(req.query))
+    });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
